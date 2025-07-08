1bb7fb28f5333db59d848a680d4bec3e85fe2dc6
Harsh Shukla <harshshukla5810@gmail.com>
2025-07-08 16:38:19 +0530

    done assignment

--- week-5/level-1/simple-react-app/src/App.jsx
+++ week-5/level-1/simple-react-app/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import Card from './components/Card';
 
 function App() {
+    return(
+        <Card name="John Doe" description="Software Engineer" interests={["Coding", "Music", "Gaming"]} />
+    )
 }
 
 export default App
--- week-5/level-1/simple-react-app/src/components/Card.jsx
+++ week-5/level-1/simple-react-app/src/components/Card.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div>
-      
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <h2>Interests</h2>
+      <ul>{props.interests.map((interest)=>{
+        return <li>{interest}</li>
+      })}</ul>
+
     </div>
   )
 }
